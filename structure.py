66313698e413d0b996ea04c9fe690973126389d9
rsoeum <49493013+rsoeum@users.noreply.github.com>
2025-03-02 20:30:33 -0800

    hero selection

--- structure.py
+++ structure.py
@@ -49,7 +49,9 @@ def player_choice(username):
         choice = input(f"\nStarnger:Sir {username}, will you help us and fight againt the waves? (yes or no): ").strip().lower()
         if choice == "yes":
             print("\nStranger:Thank you brave Hero!. For your willness to help us in this endevor we will provide the land most sacred treasuer.")
-            
+            time.sleep(2)
+            hero_type= hero_selection()
+            print(f"You are now the {hero_type}.")
 
             break
 
@@ -65,8 +67,28 @@ def player_choice(username):
         else:
             print("Invaild option")
 def hero_selection():
-    while True:
         print("Choose the type of hero you want to be.")
+        heroes = {
+            "1": "Shield Hero",
+            "2":"Sword Hero", 
+            "3":"Spear Hero",
+            "4":"Bow Hero", 
+            "5": "Magic Hero"
+
+        }
+        for key, value in heroes.items():
+            print(f"[{key}]{value}")
+        while True:
+
+            choice = input ("Enter the number for the Hero you want to be (1-5)").strip()
+
+            if choice in heroes:
+                selected_hero = heroes[choice].split(" - ")[0]
+                print(f"you have chosen {selected_hero}. Goodluck on your journey to save us from the Waves")
+                return selected_hero
+
+            else:
+                print("invalid selection. Choose (1-5)")
 
 
 if __name__ == "__main__":
