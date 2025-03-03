d776d1f0d5618cff35e0283923e651d44d1d6445
rsoeum <49493013+rsoeum@users.noreply.github.com>
2025-03-02 21:04:43 -0800

    items class

--- items.py
+++ items.py
@@ -1,7 +1,12 @@
 class Item:
-    def __init__(self,name,description,slot,damage,bonus_attack,bonus_defense, max_hp):
+    def __init__(self,name,description,slot,damage,bonus_attack,bonus_defense, max_hp,bonus_magic):
         self.name=name
         self.description = description
         self.slot=slot
         self.damage = damage
-        
+        self.bonus_attack=bonus_attack
+        self.bonus_defense=bonus_defense
+        self.bonus_magic=bonus_magic
+        self.max_hp=max_hp
+
+
