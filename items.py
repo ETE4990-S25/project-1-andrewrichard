b2c5be5ba4814c680c8d19d6e844bed8ff22ea0d
andrewm3412 <77042796+andrewm3412@users.noreply.github.com>
2025-03-11 03:28:32 -0700

    Update items.py

--- items.py
+++ items.py
@@ -1,48 +1,45 @@
-class Item:
-    def __init__(self,name,description,slot,damage,bonus_attack,bonus_defense, max_hp,bonus_magic):
-        self.name=name
-        self.description = description
-        self.slot=slot
-        self.damage = damage
-        self.bonus_attack=bonus_attack
-        self.bonus_defense=bonus_defense
-        self.bonus_magic=bonus_magic
-        self.max_hp=max_hp
+from items import FirstItems
 
-def __str__(self):
-    return(f"{self.name}({self.slot})\n"
-           f"{self.description}\n"
-           f"Damage:{self.damage}, Defense:{self.bonus_defense}, Magic:{self.bonus_magic}, HP:{self.max_hp}") 
+class Heroes:
+    def __init__(self,name,hero_class,health,attack,defense,):
+        self.name = name
+        self.hero_class =hero_class
+        self.health=health
+        self.attack=attack
+        self.defense=defense
+        self.inventory=FirstItems.get(hero_class,[])
+    
+
+    def show_stats(self):
+        print("\n" + "=" *30)
+        print(f"{self.name} the {self.hero_class}")
+        print("-" *30)
+        print(f"Health: {self.health}")
+        print(f"Attack: {self.attack}")
+        print(f"Defense: {self.defense}")
+        print("=" * 40)
+
+        print(" Inventorty:".center(40))
+
+        total_slots = 5
+        items_list = [f"[{item.name}]" for item in self.inventory]
+        inventory_slots = " ".join(items_list + ["[Empty]"] * (total_slots - len(items_list)))
+        print(inventory_slots)
+
+class ShieldHero(Heroes):
+    def __init__(self,name):
+        super().__init__(name, "Shield Hero",health=150,attack=5,defense=20,)
+
+class SwordHero(Heroes):
+    def __init__(self,name):
+        super().__init__(name, "Sword Hero",health=120,attack=15,defense=10)
 
 
-FirstItems={
-    "Shield Hero": [
+class SpearHero(Heroes):
+    def __init__(self,name):
+        super().__init__(name, "Spear Hero",health=110,attack=20,defense=5)
 
-        Item("Wood Shield", "Shield made from the Grand Oak Tree", "Weakhand",0,0,10,0,0),
-<<<<<<< HEAD
-        
-=======
-        Item("Wood Armor", "Armor made from the Grand Oak Tree", "Body", 0,0,5,0,0)
->>>>>>> d6c3ad74f81b7f402b4718b2a6491df33c61cb9b
-    ],
-
-    "Sword Hero":[ 
-        Item("Stone Sword", "Sword made from swordsmith from local village", "StrongHand",5,2,1,0,0),
-    ],
-
-    "Bow Hero": [
-        Item("Hunter's Bow","Bow used from the village's most skilled hunters","Stronghand",5,2,0,0,0),
-        Item("Arrows","set of arrows for long range attacks","accessory",0,1,0,0,0, )
-
-    ],
-
-    "Spear hero": [
-        Item("Long stone spear","Long spear made midrange attacks", "Stronghand",4,2,0,0,0),
-<<<<<<< HEAD
-=======
-        Item("Wood Armor", "Armor made from the Grand Oak Tree", "Body", 0,0,5,0,0)
->>>>>>> d6c3ad74f81b7f402b4718b2a6491df33c61cb9b
-
-    ]
-}
+class BowHero(Heroes):
+    def __init__(self,name):
+        super().__init__(name, "Bow Hero",health=100,attack=13,defense=5)
 
