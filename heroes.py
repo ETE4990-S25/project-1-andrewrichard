cf46165fa2977a416e9547c28077527bf13d7190
rsoeum <49493013+rsoeum@users.noreply.github.com>
2025-03-11 19:58:01 -0700

    added monsters

--- heroes.py
+++ heroes.py
@@ -39,6 +39,36 @@ class BowHero(Heroes):
         super().__init__(name, "Bow Hero",health=100,attack=13,defense=5)
 
 
+class Monster:
+    def __init__(self, name, health, attack,defense, reward):
+        self.name=name
+        self.health=health
+        self.attack=attack
+        self.defense=defense
+        self.reward=reward
+
+class Goblin(Monster):
+    def __init__(self):
+        super().__init__(name=Goblin, health=30, attack=10,defense=5,reward=3)
+
+class Wolf(Monster):
+    def __init__(self):
+        super().__init__(name=Wolf, health=40, attack=15,defense=10,reward=5)
+
+class Skeleton(Monster):
+    def __init__(self):
+        super().__init__(name=Skeleton, health=45, attack=20,defense=5,reward=5)
+
+class Spider(Monster):
+    def __init__(self):
+        super().__init__(name=Spider, health=25, attack=10,defense=5,reward=2)
+
+class Ogre(Monster):
+    def __init__(self):
+        super().__init__(name=Ogre, health=50, attack=30,defense=15,reward=6)
+
+
+
 
 
 
